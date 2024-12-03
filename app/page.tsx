eaaa034bc5b8a86dfbb418ebffa8f9e4840e7584
Leon-kings <leonakingeneye2002@gmail.com>
2024-12-03 10:25:34 -0800

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,65 @@
 import React from "react";
 import Footer from "./components/Footer";
+import Link from "next/link";
+import Image from "next/image";
+import side from '../public/assets/1.svg'
 const Home: React.FC = () => {
   return (
-    <div className="container ">
-        <div className='back py-6 bg-slate-300 grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1'>
-<h2 className="p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-  Alias doloremque fuga repudiandae quam amet nihil sapiente quia non a unde ad, 
-  necessitatibus voluptates adipisci quod quaerat? Quia itaque et temporibus.</h2>
-  <h2 className="p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-  Alias doloremque fuga repudiandae quam amet nihil sapiente quia non a unde ad, 
-  necessitatibus voluptates adipisci quod quaerat? Quia itaque et temporibus.</h2>
-  <h2 className="p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-  Alias doloremque fuga repudiandae quam amet nihil sapiente quia non a unde ad, 
-  necessitatibus voluptates adipisci quod quaerat? Quia itaque et temporibus.</h2>
-  <h2 className="p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-  Alias doloremque fuga repudiandae quam amet nihil sapiente quia non a unde ad, 
-  necessitatibus voluptates adipisci quod quaerat? Quia itaque et temporibus.</h2>
-  <h2 className="p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-  Alias doloremque fuga repudiandae quam amet nihil sapiente quia non a unde ad, 
-  necessitatibus voluptates adipisci quod quaerat? Quia itaque et temporibus.</h2>
-  <h2 className="p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-  Alias doloremque fuga repudiandae quam amet nihil sapiente quia non a unde ad, 
-  necessitatibus voluptates adipisci quod quaerat? Quia itaque et temporibus.</h2>
+    <div className="container py-4">
+       {/* Animation Video Container */}
+       <div className="page-video-container absolute inset-0 z-50 flex items-center justify-center bg-black">
+        <video
+          className="w-full h-full object-cover"
+          autoPlay
+          muted
+          loop
+          playsInline
+        >
+              <source src="/Y.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+        </video>
+       </div>
+         {/*  */}
+         {/*  */}
+      {/* heading */}
+      <div className="relative">
+      <div style={{ backgroundImage: `url(${'https://wallpapercat.com/w/full/b/c/3/5818107-1920x1080-desktop-hd-cool-car-wallpaper-photo.jpg'})`, backgroundSize: 'cover',height:'80vh',width:'100%' }}>
+         <div className="headingtext">
+        
+            <div className="front">
+              {/* space to fit a car */}
+              <p className="title "></p>
+              <p className="title"></p>
+              {/* space to fit a car ends*/}
+              <h2 className="title text-center p-4 py-8 text-white">GET YOUR DESIRED CAR IN RESONABLE PRICE</h2>
+
+              <h4 className="title text-center justify-center text-green-300 py-20">
+                Leon is happy to serve you Your favourate Cars ,
+                Choose The Best Cars You want and Your repairment Kits.
+              </h4>
+              <div className="stitle justify-center p-5">
+                <Link href={'/contact'}>
+                  <button className="button p-2 bg-blue-500 text-center">Contact us</button>
+                </Link>
+              </div>
+            </div>
+
         </div>
-        <h2 className="p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-  Alias doloremque fuga repudiandae quam amet nihil sapiente quia non a unde ad, 
-  necessitatibus voluptates adipisci quod quaerat? Quia itaque et temporibus.</h2>
-  <div className="container">
+         </div>
+<Image src={side} alt="" />
+      </div>
+      {/*  */}
+      {/*  */}
+      <div className="container">
+ 
+      </div>
+      {/* footer side */}
+     <div className="container py-4">
     <Footer/>
+     </div>
+     {/*  */}
+     {/*  */}
   </div>
-    </div>
   );
 };
 
